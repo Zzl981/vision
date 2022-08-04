fee6878554e5c82af909d01fd749ddb0d26688d3
xiaolinzi <1782576830@qq.com>
2022-08-04 23:41:36 +0800

    增加了销量趋势的路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import SellerPage from '@/views/SellerPage'
 import MapPage from '@/views/MapPage'
+import TrendPage from '@/views/TrendPage'
 
 Vue.use(VueRouter)
 
@@ -13,6 +14,10 @@ const routes = [
   {
     path: '/mappage',
     component: MapPage
+  },
+  {
+    path: '/trendpage',
+    component: TrendPage
   }
 ]
 
