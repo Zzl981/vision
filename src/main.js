ac4957965763cba77ea2996eaaa8d12a25cbc21e
xiaolinzi <1782576830@qq.com>
2022-08-04 23:42:30 +0800

    引入了全局的字体样式表

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+// 引入全局样式表
 import './assets/css/globel.less'
+// 引入字体样式表
+import './assets/font/iconfont.css'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api'
 // 将全局的echarts对象挂载到Vue的原型对象，其它组件通过this.$echarts调用
 Vue.prototype.$echarts = window.echarts
